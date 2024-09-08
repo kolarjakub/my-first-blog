b621d63ef0a2ef865080eb91894ff9dff73f64f0
Jakub Kolar <jakub03kolar@gmail.com>
2024-09-08 10:18:51 +0200

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+#tecka znamena, ze je to ve stejnem adresari
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts}) #kuri zavorky jsou to, co predavame sablne a pojmeonavni
